16c288ed8d5af5f65b5282139642a287ff0121ce
dev_gulamrosul <gulamrosul037@gmail.com>
2026-04-07 04:53:58 +0600

    added custom appbar and plan

--- lib/feature/navbar/navbar_wid/navbar_main_shell.dart
+++ lib/feature/navbar/navbar_wid/navbar_main_shell.dart
@@ -1,4 +1,5 @@
 import 'package:deanash_28/feature/home/screem/home_page.dart';
+import 'package:deanash_28/feature/seven_days_premium/screen/seven_days_premiu_plan_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,11 +7,18 @@ import '../controller/navbar_controller.dart';
 import 'navbar.dart';
 
 class MainShell extends StatelessWidget {
-  final NavController controller = Get.put(NavController());
-  final List<Widget> screens = [HomePage()];
-  MainShell({super.key});
+  const MainShell({super.key});
   @override
   Widget build(BuildContext context) {
+    final NavController controller = Get.put(NavController());
+    final List<Widget> screens = [
+      HomePage(),
+      SevenDaysPremiuPlanPage(),
+      HomePage(),
+      SevenDaysPremiuPlanPage(),
+      HomePage(),
+      SevenDaysPremiuPlanPage(),
+    ];
     return Scaffold(
       body: Obx(
         () => IndexedStack(
